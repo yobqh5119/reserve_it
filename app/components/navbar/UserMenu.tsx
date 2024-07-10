7c00b20d24d63edb64ac27b94afe1aee04793073
yoobin <yoobin.hong@ingka.ikea.com>
2024-07-09 21:42:41 -0400

    adding toggling effect for the mobile menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,18 @@
 'use client'
-import React from 'react'
+
+import React, { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import { FaRegUser } from 'react-icons/fa'
+import MenuItem from './MenuItem'
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
+
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
@@ -12,13 +20,22 @@ const UserMenu = () => {
           Reserve It your home
         </div>
 
-        <div onClick={() => { }} className='p-4 md:py-2 md: px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
+        <div onClick={toggleOpen} className='p-4 md:py-2 md: px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
           <AiOutlineMenu />
           <div className="hidden md:block">
             <FaRegUser />
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem label='Listing' onClick={() => { }} />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
